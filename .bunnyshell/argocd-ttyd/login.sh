978be06ed6694ef725fee1dd43b8634fb89de2be
mihaichitic <m.chitic+github@gmail.com>
2024-10-23 20:07:53 +0300

    Update login.sh
    for argocd-server without server.insecure=true
    
    Signed-off-by: mihaichitic <m.chitic+github@gmail.com>

--- .bunnyshell/argocd-ttyd/login.sh
+++ .bunnyshell/argocd-ttyd/login.sh
@@ -6,15 +6,15 @@ initialPass=${ARGOCD_INITIAL_PASS}
 
 if [[ ! -z "$initialPass" ]]; then
   echo "logging in with initial pass..."
-  argocd login --username admin --password $initialPass --insecure --skip-test-tls $serverAddr
+  argocd login --username admin --password $initialPass --plaintext $serverAddr
   if [ $? -eq 0 ]; then
      echo "setting new pass..."
      argocd account update-password --current-password $initialPass --new-password $newPass
   else
      echo "logging in with new pass..."
-     argocd login --username admin --password $newPass --insecure --skip-test-tls $serverAddr
+     argocd login --username admin --password $newPass --plaintext $serverAddr
   fi
 else
   echo "logging in with new pass..."
-  argocd login --username admin --password $newPass --insecure --skip-test-tls $serverAddr
+  argocd login --username admin --password $newPass --plaintext $serverAddr
 fi
